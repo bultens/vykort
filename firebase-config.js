dccb2f1fb806c010f86e75304724d014e6f37d38
bultens <github@bultens.net>
2025-09-01 11:03:42 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB52hRH8vfCKFLCAzPCEaQM05lKHgYq-hE",
+    authDomain: "kumlaskytte.firebaseapp.com",
+    projectId: "kumlaskytte",
+    storageBucket: "kumlaskytte.firebasestorage.app",
+    messagingSenderId: "571676149705",
+    appId: "1:571676149705:web:a0ace868f4680298a78a18",
+    measurementId: "G-KSJ9NLNDBX"
+};
